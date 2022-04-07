5b2cb44df84e6e238815658f206e90fb828e8352
Ritu Vyas <rituvyas045@gmail.com>
2022-04-07 20:45:45 +0530

    first commit

---
+++ src/test/java/qaclickacademy/MavenJava/RestAPI.java
@@ -0,0 +1,21 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+///this won't run bcoz Test keyword is used in naming the class file
+v
+public class RestAPI {
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+	
+
+}
+ 
